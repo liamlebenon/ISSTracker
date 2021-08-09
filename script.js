1708a2195541abf9116ebf270dfce40bade1ccfc
Liam <liam.lebenon@gmail.com>
2021-08-09 15:25:49 +0100

    add files for v1

---
+++ script.js
@@ -0,0 +1,36 @@
+let pos;
+var markersArray = [];
+
+
+const ISSLocation = () => {
+    setInterval(async () => {
+        const response = await fetch('https://api.wheretheiss.at/v1/satellites/25544');
+        let iss = await response.json();
+        pos = {lat: iss.latitude, lng: iss.longitude}
+    }, 5000)
+}
+
+ISSLocation();
+let map;
+
+function initMap() {
+    setTimeout(() => {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: pos,
+    zoom: 8,
+  });
+  const marker = 
+    setInterval(() => {
+        marker.setMap(null)
+        new google.maps.Marker({
+            position: pos,
+            map: map,
+        })
+        console.log(pos);
+    }, 5100);
+  console.log(pos)
+}, 5500)
+}
+
+
+
